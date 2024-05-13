9871be13d4cec5193096dcf152a4c49111d36618
soda-sy <qkrthdud2124@naver.com>
2024-05-13 10:46:45 +0900

    [ADD] init Files

---
+++ src/main/java/com/example/indiebeauty/IndiebeautyApplication.java
@@ -0,0 +1,13 @@
+package com.example.indiebeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndiebeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndiebeautyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/indiebeauty/IndiebeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.indiebeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndiebeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
