82d2580a2f0eb50d66292dfbbfc9c1e15acde3b3
JIMIN HWANG <hwang0010@naver.com>
2024-05-28 11:46:41 +0900

    fix: HTML Thymeleaf로 변경
    resources/static : 정적 파일 (css, fonts, img, js)
    resources/templates : Thymeleaf 파일

--- src/main/java/com/example/indiebeauty/WebMvcConfig.java
+++ src/main/java/com/example/indiebeauty/WebMvcConfig.java
@@ -1,30 +1,30 @@
-package com.example.indiebeauty;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-	@Autowired
-	@Qualifier(value = "signonInterceptor")
-	private HandlerInterceptor interceptor;
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-//		registry.addViewController("/shop/index.do").setViewName("index");
-	}
-	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(interceptor)
-//				.addPathPatterns("/shop/editAccount.do", "/shop/listOrders.do",
-//					"/shop/viewOrder.do", "/shop/newOrder.do");		
-	}
-	
-}
+//package com.example.indiebeauty;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+//	@Autowired
+//	@Qualifier(value = "signonInterceptor")
+//	private HandlerInterceptor interceptor;
+//
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+////		registry.addViewController("/shop/index.do").setViewName("index");
+//	}
+//	
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+////		registry.addInterceptor(interceptor)
+////				.addPathPatterns("/shop/editAccount.do", "/shop/listOrders.do",
+////					"/shop/viewOrder.do", "/shop/newOrder.do");		
+//	}
+//	
+//}
