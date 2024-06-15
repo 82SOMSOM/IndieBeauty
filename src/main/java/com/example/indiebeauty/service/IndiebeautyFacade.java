3a55c124455edbd5c95273ef67f59a15d70cdd9f
seplease <seplease@naver.com>
2024-06-16 02:32:16 +0900

    0616 DB 사용 Cart 부분 모두 삭제

--- src/main/java/com/example/indiebeauty/service/IndiebeautyFacade.java
+++ src/main/java/com/example/indiebeauty/service/IndiebeautyFacade.java
@@ -2,7 +2,6 @@ package com.example.indiebeauty.service;
 
 import java.util.List;
 
-import com.example.indiebeauty.domain.CartItem;
 import com.example.indiebeauty.domain.Item;
 import com.example.indiebeauty.domain.UserInfo;
 
@@ -14,12 +13,4 @@ public interface IndiebeautyFacade {
 	void insertUserInfo(UserInfo userinfo);
 	void updateUserInfo(UserInfo userinfo);
 //	List<String> getUserIdList();
-
-	// Cart
-	void updateCartItem(CartItem ci);
-	boolean isItemInStock(int workingItemId);
-	void insertCartItem(CartItem ci);
-	void deleteCartItem(CartItem ci);
-
-	Item getItem(int workingItemId);
 }
