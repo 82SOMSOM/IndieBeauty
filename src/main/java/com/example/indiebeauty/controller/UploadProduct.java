075245589a470eb05e63b6fb5ebeea0357178a50
JIMIN HWANG <hwang0010@naver.com>
2024-06-21 20:14:35 +0900

    feature: validation 기능 구현
    상품 업로드에 필요한 validation 구현했습니다.

--- src/main/java/com/example/indiebeauty/controller/UploadProduct.java
+++ src/main/java/com/example/indiebeauty/controller/UploadProduct.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,11 +22,24 @@ import lombok.ToString;
 @ToString
 public class UploadProduct {
 	private int categoryId;
+	
+	@NotEmpty(message = "Please wirte down the name of product.")
 	private String name;
+	
 	private String description;
+	
 	private MultipartFile titleImage;
+	
+	@Size(max = 4, message = "Detail images must be less than or equal to 4.")
 	private List<MultipartFile> detailImageList;
+	
 	private Date date;
+	
+    @Min(value = 1, message = "The price must be at least 1 KRW.")
+	@Max(value = 1000000, message = "The price must be less than or equal to 1,000,000.")
 	private int price;
+	
+    @Min(value = 1, message = "The stock must be at least one.")
+	@Max(value = 100000, message = "The stock quantity must be less than or equal to 100,000.")
 	private int stock;
 }
