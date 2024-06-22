005090011f4a4d615a89fa003aa7b0c6752a60bf
JIMIN HWANG <hwang0010@naver.com>
2024-06-22 20:52:10 +0900

    fix: IndiebeautyImpl 수정
    주석 정리 및 오류 수정

--- src/main/java/com/example/indiebeauty/service/IndiebeautyImpl.java
+++ src/main/java/com/example/indiebeauty/service/IndiebeautyImpl.java
@@ -4,14 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.indiebeauty.domain.Product;
 import com.example.indiebeauty.domain.Review;
 import com.example.indiebeauty.domain.SellerInfo;
 import com.example.indiebeauty.domain.UserInfo;
-import com.example.indiebeauty.repository.FavoritesRepository;
 import com.example.indiebeauty.repository.OrdersRepository;
 import com.example.indiebeauty.repository.ProductRepository;
 import com.example.indiebeauty.repository.ReviewRepository;
-import com.example.indiebeauty.repository.SellerEventsRepository;
 import com.example.indiebeauty.repository.SellerRepository;
 import com.example.indiebeauty.repository.UserRepository;
 
@@ -27,8 +26,6 @@ public class IndiebeautyImpl implements IndiebeautyFacade {
 	private ProductRepository productRepository;
 	@Autowired
 	private OrdersRepository ordersRepository;
-	@Autowired
-	private SellerEventsRepository sellerEventsRepository;
 	
 	public UserInfo getUserInfo(String userid) {
 		return userRepository.getReferenceById(userid);
@@ -59,11 +56,10 @@ public class IndiebeautyImpl implements IndiebeautyFacade {
 		return productRepository.existsByProductIdAndStockGreaterThan(workingProductId, 0);
 	}
 	
-//	@Override
-//	public Product getProduct(int workingProductId) {
-////		return productRepository.getReferenceById(workingProductId);
-//		return null;
-//	}
+	@Override
+	public Product getProduct(int workingProductId) {
+		return null;
+	}
 	
 	
 // 	review
@@ -94,13 +90,5 @@ public class IndiebeautyImpl implements IndiebeautyFacade {
 	public boolean existsSellerId(String sellerid) {
 	    return sellerRepository.existsById(sellerid);
 	}
-//	public void deleteAllSellerRelatedData(String sellerid) {
-//		sellerEventsRepository.deleteBySellerId(sellerid);
-//		productRepository.deleteBySellerId(sellerid);
-//	}
-//	public void deleteSellerInfo(String sellerId) {
-//	    deleteAllSellerRelatedData(sellerId); // 자식 레코드 먼저 삭제
-//	    sellerRepository.deleteById(sellerId); // 그 다음 사용자 레코드 삭제
-//	}
 
 }
